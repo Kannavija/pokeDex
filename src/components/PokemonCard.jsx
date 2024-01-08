69ea96c94bf391bdbf3e5c85ee3086d2771412e8
kanna 77 <kannavija@gmail.com>
2024-01-08 16:54:43 +0100

    pokemon lis

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,8 +1,28 @@
 function PokemonCard() {
     return <figure>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur" />
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur"/>
     <figcaption>Bulbasaur</figcaption>
     </figure>
-}
 
-export default PokemonCard;
+     if (pokemon.imgSrc === 'true') {
+      const pokemon = pokemonList[0];
+      return <figure>{pokemon.name}
+      <img src = {pokemon.imgSrc} alt= {pokemon.name}/>
+      </figure>}
+  } else if {
+    <p>'???'</p>
+  }
+
+
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+
+  export default PokemonCard;
