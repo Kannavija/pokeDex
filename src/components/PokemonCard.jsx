553938a8e3f64da2b07a84a8311197799f44d9fb
Kanna <152877516+Kannavija@users.noreply.github.com>
2024-01-10 12:37:54 +0100

    Update PokemonCard.jsx

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,28 +1,25 @@
 function PokemonCard() {
-    return <figure>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur"/>
-    <figcaption>Bulbasaur</figcaption>
-    </figure>
-
-     if (pokemon.imgSrc === 'true') {
       const pokemon = pokemonList[0];
-      return <figure>{pokemon.name}
-      <img src = {pokemon.imgSrc} alt= {pokemon.name}/>
-      </figure>}
-  } else if {
-    <p>'???'</p>
-  }
-
-
-const pokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
-
-  export default PokemonCard;
+      return (
+        <figure>
+          {pokemon.imgSrc ? (
+            <img src={pokemon.imgSrc} alt={pokemon.name} />
+          ) : (
+            <p>???</p>
+          )}
+          <figcaption>{pokemon.name}</figcaption>
+        </figure>
+      );
+    }
+    export default PokemonCard;
+    
+    const pokemonList = [
+      {
+        name: "bulbasaur",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+      },
+      {
+        name: "mew",
+      },
+    ];
