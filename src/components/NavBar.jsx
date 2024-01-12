7ab903c2b3844265b1ff490391144b95ec184e16
Kanna <152877516+Kannavija@users.noreply.github.com>
2024-01-12 15:32:22 +0100

    repeat an component with map

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,24 +1,17 @@
-
 // eslint-disable-next-line react/prop-types
 function NavBar({setPokemonIndex, pokemonIndex, pokemonList}) {
-
-    let previousClic = () => {
-        if (pokemonIndex > 0) {
-        setPokemonIndex(pokemonIndex - 1)
-        }
-      }
-    
-      let nextClic = () => {
-        // eslint-disable-next-line react/prop-types
-        if (pokemonIndex < pokemonList.length - 1)
-        setPokemonIndex(pokemonIndex + 1)
-      }
-      return (
-        <div>
-            <button onClick={previousClic}>precedent</button>
-            <button onClick={nextClic}>suivant</button>
-        </div>
-      )
-}
-
-export default NavBar;
+    return (
+      <div>
+        {pokemonList.map((pokemon) => (
+          <li key={pokemon.name}>
+            <button onClick={() => setPokemonIndex(pokemonList.indexOf(pokemon))}>
+              {pokemon.name}
+            </button>
+          </li>
+        ))}
+      </div>
+    );
+  }
+  
+  export default NavBar;
+  
